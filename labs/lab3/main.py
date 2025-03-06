4c84cbc262c3d0d23022c870e38c6b9c0f17b451
Joshua Auw-Yang <joshua.auwyang@gmail.com>
2025-03-05 21:32:33 -0500

    finished working code

--- labs/lab3/main.py
+++ labs/lab3/main.py
@@ -1,7 +1,9 @@
 from filterpy.kalman import ExtendedKalmanFilter
 import matplotlib.pyplot as plt
+import scipy.io
 import numpy as np
 from enum import Enum
+from typing import List
 import math
 
 # cd Desktop\Projects\Programs\uw-courses\mte546-sensor-fusion\
@@ -24,40 +26,30 @@ T_ms = 0.0383
 x_o = 30  # [cm]
 x_f = 70
 t_f = 10  # [sec]
-motion = MOTION_TYPE.RANDOM
+motion = MOTION_TYPE.TILTED
 
-# 1.0e07
-Q_mat_sim = 1.0e07 * np.array(
-    [
-        [0.000000292810336, 0.000005331742902, 0.000103885699918],
-        [0.000005331742902, 0.000970437150569, 0.028065056461569],
-        [0.000103885699918, 0.028065056461569, 1.462534098876549],
-    ]
-)
-# Q_mat = 0.000001 * np.eye(3)
 
-Q_mat = 1.0e07 * np.array(
-    [
-        [0.000237653309536, 0, 0],
-        [0, 0.000970437150569, 0],
-        [0, 0, 1.462534098876549 * 1.0e-07],
-    ]
-)
-
-# Q_mat = np.array(
+# New Qs
+Q_mat = np.array([[4, 5.76, 0.2], [5.76, 10, 0.9], [0.2, 0.9, 0.01]])  # GOOOOD
+# Q_mat = np.array(  # JASON'S BAD ONE
 #     [
-#         [1.0e06 * 0.000237653309536, 0, 0],
-#         [0, 1, 0],
-#         [0, 0, 0.5],
+#         [0.018548541771278, 0.032342390287647, -0.605962817715705],
+#         [0.032342390287647, 1.732714927238333, 29.801855172595230],
+#         [0.605962817715705, 29.801855172595230, 1.551392820965659e03],
 #     ]
 # )
 
+Q_mat_sim = Q_mat
+
 R_mat = np.array([[0.000161625, 0], [0, 0.0011362222]])
-# R_mat = np.eye(2)
 
+# simulated measurement noise
+# R1_mat = np.array([[0.000161625]])
+# R2_mat = np.array([[0.0011362222]])
 
-R1_mat = np.array([[0.000161625]])
-R2_mat = np.array([[0.0011362222]])
+# condition 3
+R1_mat = np.array([[5]])
+R2_mat = np.array([[5]])
 
 
 ## Noise Helper Functions
@@ -155,8 +147,10 @@ def fuse_sensors(v_med: float, v_long: float):
     pass
 
 
-## Plot graphs
-def plot_states_overtime(states: np.array, true_pos: np.array, t_f: float):
+## Plot graphs: all 3 states, as well as the certainty of the estimate
+def plot_states_uncertainty_overtime(
+    states: np.array, covariances: List, true_pos: np.array, t_f: float, title: str = ""
+):
     time_range = np.arange(0, t_f + T_ms, T_ms)
 
     labels = [
@@ -168,17 +162,32 @@ def plot_states_overtime(states: np.array, true_pos: np.array, t_f: float):
 
     # Stack the true state on top of the estimated states
     states = np.vstack((true_pos, states))
-    plt.figure(figsize=(8, 5))
+    fig, axes = plt.subplots(2, 1, figsize=(12, 8))
+    if title != "":
+        fig.suptitle(title)
+
     # for state_i in range(states.shape[0]):
     for state_i in range(4):
-        plt.plot(
+        axes[0].plot(
             time_range[: len(time_range) - 1], states[state_i, :], label=labels[state_i]
         )
 
-    plt.xlabel("Time [s]")
-    plt.ylabel("State")
-    plt.legend()
-    plt.grid(True)
+    axes[0].set_title("Simulated Object States vs. Estimated States")
+    axes[0].set_xlabel("Time [s]")
+    axes[0].set_ylabel("State")
+    axes[0].legend()
+    axes[0].grid(True)
+
+    # Visualize the certainty of the estimate using the trace of P (sum of the eigenvalues)
+    traces = [np.trace(p) for p in covariances]
+    axes[1].plot(time_range[: len(time_range) - 1], traces)
+
+    axes[1].set_title("Uncertainty of Estimate Over Time")
+    axes[1].set_xlabel("Time [s]")
+    axes[1].set_ylabel("Traces of P Matrices")
+    axes[1].grid(True)
+
+    plt.tight_layout()
     plt.show()
 
 
@@ -205,47 +214,187 @@ def Hx(x, *args):
     return np.array([z1, z2]).reshape(2, 1)
 
 
+# ===========================================================
+# Experimental Data
+
+
+def load_matlab_data(root_path: str, cases: list):
+    different_traj_data = {}
+    case_i = 1
+    for case in cases:
+        curr_case = scipy.io.loadmat(root_path + case)
+        curr_case_data = curr_case["data"]
+        curr_case_time = curr_case["time"]
+
+        match case_i:
+            case 1:
+                different_traj_data[MOTION_TYPE.STATIONARY] = curr_case_data
+                different_traj_data["time"] = curr_case_time
+            case 2:
+                different_traj_data[MOTION_TYPE.SMOOTH] = curr_case_data
+            case 3:
+                different_traj_data[MOTION_TYPE.NONLINEAR] = curr_case_data
+            case 4:
+                different_traj_data[MOTION_TYPE.RANDOM] = curr_case_data
+            case 5:
+                different_traj_data[MOTION_TYPE.TILTED] = curr_case_data
+        case_i += 1
+
+    return different_traj_data
+
+
+def plot_raw_exp_data_useless(different_traj_data: dict, case: MOTION_TYPE):
+    data = different_traj_data[case]
+    time = different_traj_data["time"]
+
+    plt.figure(figsize=(12, 8))
+    plt.plot(time, data[:, 0])
+    plt.plot(time, data[:, 1])
+
+    plt.title("Raw Experimental Data")
+    plt.xlabel("Time [s]")
+    plt.ylabel("Sensor Reading [V]")
+    plt.grid(True)
+    plt.show()
+    
+    
+def plot_states_uncertainty_overtime_exp(
+    states: np.array, covariances: List, t_f: float, title: str = ""
+    ):
+    time_range = np.arange(0, t_f + T_ms, T_ms)
+
+    labels = [
+        "Est. Position [cm]",
+        "Est. Velocity [cm/s]",
+        "Est. Acceleration [cm/s^2]",
+    ]
+
+    # Stack the true state on top of the estimated states
+    fig, axes = plt.subplots(2, 1, figsize=(12, 8))
+    if title != "":
+        fig.suptitle(title)
+
+    for state_i in range(3):
+        axes[0].plot(
+            time_range[: len(time_range) - 2], states[state_i, :], label=labels[state_i]
+        )
+    # Start pos
+    axes[0].scatter(0, x_o, marker='o', color='red', label=f"True Start Position, x_o")
+    # End pos
+    axes[0].scatter(time_range[-3], x_f, marker='o', color='blue', label=f"True Final Position, x_f")
+
+    axes[0].set_title("Estimated States from Experimental Data")
+    axes[0].set_xlabel("Time [s]")
+    axes[0].set_ylabel("State")
+    axes[0].legend()
+    axes[0].grid(True)
+
+    # Visualize the certainty of the estimate using the trace of P (sum of the eigenvalues)
+    traces = [np.trace(p) for p in covariances]
+    axes[1].plot(time_range[: len(time_range) - 2], traces)
+
+    axes[1].set_title("Uncertainty of Estimate Over Time")
+    axes[1].set_xlabel("Time [s]")
+    axes[1].set_ylabel("Traces of P Matrices")
+    axes[1].grid(True)
+
+    plt.tight_layout()
+    plt.show()
+    
+    
+    
+
+
 def main():
     estimated_states = []
+    covariances = []
     true_pos = []
-
-    # Q_mat = 1.0e07 * np.array(
-    #     [
-    #         [0.000000292810336, 0.000005331742902, 0.000103885699918],
-    #         [0.000005331742902, 0.000970437150569, 0.028065056461569],
-    #         [0.000103885699918, 0.028065056461569, 1.462534098876549],
-    #     ]
-    # )
 
     ekf = ExtendedKalmanFilter(NUM_STATES, NUM_MEASUREMENT_IN)
     ekf.x = np.array([x_o, 0, 0])  # initial states to 0 except for position
-    ekf.P = Q_mat  # initialization choice
+    # ekf.x = np.array([25, 1, 0])  # condition 1: close to true init state
+    # ekf.x = np.array([60, 5, 1])  # condition 1: far from true init state
+
+    ekf.P = Q_mat
     ekf.R = R_mat
     ekf.Q = Q_mat
     ekf.F = np.array([[1, T_ms, 0.5 * T_ms**2], [0, 1, T_ms], [0, 0, 1]])
     ekf.H = None
 
-    # Simulation time
-    t = 0
-    while t < t_f:
-        curr_pos = actual_pos(t)
-        print(t)
-        z_1 = sensor_medium_d2v(curr_pos)
-        z_2 = sensor_long_d2v(curr_pos)
+    
+    run_simulated = False
+    if run_simulated:
+        t = 0
+        while t < t_f:
+            curr_pos = actual_pos(t)
+            true_pos.append(curr_pos)
 
-        z = np.array([z_1, z_2])  # [2, 1]
-        ekf.predict_update(z, HJacobian, Hx)
-        posterior_state = ekf.x  # ????? idk if this is the correct function
-        print(posterior_state)
+            z_1 = sensor_medium_d2v(curr_pos)
+            z_2 = sensor_long_d2v(curr_pos)
+            z = np.array([z_1, z_2])  # [2, 1]
+            ekf.predict_update(z, HJacobian, Hx)
+            posterior_state = ekf.x
+            # print(posterior_state)
 
-        true_pos.append(curr_pos)
-        estimated_states.append(posterior_state)
-        t += T_ms
+            covariance = ekf.P
+            # print(f"Cov: {covariance}")
 
-    # print(true_pos.shape)
-    plot_states_overtime(
-        np.hstack(estimated_states), np.array(true_pos).reshape(1, -1), t_f
-    )
+            estimated_states.append(posterior_state)
+            covariances.append(ekf.P)
+            t += T_ms
+
+        # print(true_pos.shape)
+        plot_states_uncertainty_overtime(
+            np.hstack(estimated_states),
+            covariances,
+            np.array(true_pos).reshape(1, -1),
+            t_f,
+            f"Trajectory Simulation ({motion.name} MOTION)",
+        )
+
+    else: # experimental data
+        experimental_data_path = "./MTE546_Lab3_Data"
+        diff_cases_paths = [
+            "/stationary_50.mat",
+            "/smooth_linear_30_to_70.mat",
+            "/variation_velocity_30_to_70.mat",
+            "/random_30_to_70.mat",
+            "/smooth_tilted_30_to_70.mat",
+        ]
+
+        exp_data = load_matlab_data(experimental_data_path, diff_cases_paths)
+        # plot_raw_exp_data_useless(exp_data, motion)
+        # print(motion)
+        # print(exp_data[motion])
+
+        curr_traj_z_vals = np.array(exp_data[motion])  # [N, 2]
+        z_exp = curr_traj_z_vals.T  # [2, N]
+        times_exp = exp_data['time']
+
+        # Simulation time
+        for i in range(len(times_exp)):
+            z = z_exp[:, i]
+            z = z.reshape(-1, 1)
+            # print(z.shape)
+
+            ekf.predict_update(z, HJacobian, Hx)
+            posterior_state = ekf.x
+
+            covariance = ekf.P
+            # print(f"Cov: {covariance}")
+
+            estimated_states.append(posterior_state)
+            covariances.append(ekf.P)
+
+        t_f = times_exp[-1]
+
+        # print(true_pos.shape)
+        plot_states_uncertainty_overtime_exp(
+            np.hstack(estimated_states),
+            covariances,
+            t_f,
+            f"Trajectory Simulation ({motion.name} MOTION)",
+        )
 
 
 if __name__ == "__main__":
